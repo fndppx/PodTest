cee2947d3895c659ec731dc019af0d5b6abf888b
weikeyan <270022748@qq.com>
2017-07-13 11:59:49 +0800

    test

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by SXJH on 2017/7/13.
+//  Copyright © 2017年 SXJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by SXJH on 2017/7/13.
+//  Copyright © 2017年 SXJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
